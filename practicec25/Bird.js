0760999248a33ddabdc9fbc04716ad4cba603ccd
Apple <apple@Apples-MacBook-Air.local>
2020-12-23 19:39:22 +0530

    hello

---
+++ practicec25/Bird.js
@@ -0,0 +1,16 @@
+class bird extends BaseClass{
+  constructor(x,y){
+super(x,y,50,50);
+this.width = 50;
+this.height = 50;
+this.image = loadImage("sprites/bird.png");
+  }
+  display(){
+
+var pos = this.body.position;
+
+pos.x = mouseX;
+pos.y = mouseY;
+super.display();
+  }
+}
---
+++ practicec25/Box.js
@@ -0,0 +1,9 @@
+class Box extends BaseClass {
+    constructor(x,y){
+     super(x,y,50,50);
+     this.width = 50;
+     this.height = 50;
+     this.image = loadImage("sprites/wood1.png");
+    }
+   
+}
